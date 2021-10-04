65eb6daaaf82a9cbf22fc96a2fa8a74a2ffb5a51
Kathleen Wine <katie.wine2@gmail.com>
2021-10-04 19:12:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "projectName",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project:"
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
