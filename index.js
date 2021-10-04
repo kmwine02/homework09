9d261e06e2974bfcbd26393906a095a64d170b00
Kathleen Wine <katie.wine2@gmail.com>
2021-10-04 19:34:39 -0400

    adding questions for inquirer prompt

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Choice = require("inquirer/lib/objects/choice");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
         name: "projectName",
-        message: "What is the name of your project?"
+        message: "What is your project's name?"
     },
     {
         type: "input",
         name: "description",
-        message: "Enter a description of your project:"
+        message: "Enter a short description of your project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?"
+    },
+    {
+        type: "input",
+        name: "repoInstructions",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "repoContributions",
+        message: "What does the user need to know about contributing to the repo?"
     }
 ];
 
@@ -20,7 +57,11 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt().then((answers => {
+
+    }))
+}
 
 // Function call to initialize app
 init();
